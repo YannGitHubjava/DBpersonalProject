82ff915e654eb6b452c1eb0136bee3f65a72327d
Federico <fedefdez1982@gmail.com>
2016-10-17 17:16:27 -0500

    Added a slightly prettier menu stored in a separate file, polished a bit on data validation and streamlined some functions to get rid of auxiliar variables

---
+++ menus.py
@@ -0,0 +1,47 @@
+def display_initial_message():
+    print(' ______________________________________________________________________________ \n'
+          '|                  WELCOME TO THE MUSIC SEARCH PROGRAM.                        |\n'
+          '|                                                                              |\n'
+          '|        COMES WITH A PRELOADED MUSIC SELECTION TO SHOW FUNTIONALITIES         |\n'
+          '|                                                                              |\n'
+          '|______________________________________________________________________________|\n')
+
+def display_music_query():
+    print(' ______________________________________________________________________________ \n'
+          '|                                                                              |\n'
+          '|      TYPE "YES" TO ENTER DATA IN THE MUSIC TABLE, "NO" TO EXIT PROGRAM       |\n'
+          '|______________________________________________________________________________|\n')
+
+def display_artist_input():
+    print(' ______________________________________________________________________________ \n'
+          '|                                                                              |\n'
+          '|        TYPE AN ARTIST NAME OR PRESS ENTER TO SKIP FOR VOICE INPUT            |\n'
+          '|______________________________________________________________________________|\n')
+
+def display_options_menu():
+    print(' ______________________________________________________________________________ \n'
+          '|                                                                              |\n'
+          '|                      1. SEARCH ALL FIELDS                                    |\n'
+          '|                      2. SEARCH IN ARTIST NAME FIELD                          |\n'
+          '|                      3. SEARCH IN ALBUM TITLE FIELD                          |\n'
+          '|                      4. SEARCH IN SONG/TRACK TITLE FIELD                     |\n'
+          '|______________________________________________________________________________|\n')
+
+def display_track_import():
+    print(' ______________________________________________________________________________ \n'
+          '|                                                                              |\n'
+          '|         WHICH TRACK WOULD YOU LIKE TO IMPORT? (TYPE "EXIT" TO QUIT)          |\n'
+          '|______________________________________________________________________________|\n')
+
+
+def display_error():
+    print(' ______________________________________________________________________________ \n'
+          '|                                                                              |\n'
+          '|          ERROR, INVALID OPTION. PLEASE INTRODUCE A VALID OPTION              |\n'
+          '|______________________________________________________________________________|\n')
+
+def display_error_2():
+    print(' ______________________________________________________________________________ \n'
+          '|                                                                              |\n'
+          '|          INVALID SELECTION, WHICH TRACK WOULD YOU LIKE TO IMPORT             |\n'
+          '|______________________________________________________________________________|\n')
